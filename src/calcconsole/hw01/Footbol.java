dcd2cfd22371ae3ab00062141270d53d23c3b821
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2019-03-27 02:51:52 +0200

    remove from hw02

--- src/calcconsole/hw01/Footbol.java
+++
@@ -1,9 +0,0 @@
-package calcconsole.hw01;
-
-public class Footbol {
-    public int getResultOfBettings(int firstTeamGoals, int secondTeamGoals, int userFirstGoal, int userSecondGoal) {
-        return (firstTeamGoals == userFirstGoal && secondTeamGoals == userSecondGoal) ? 2 :
-                (firstTeamGoals > secondTeamGoals && userFirstGoal > userSecondGoal) ||
-                        (firstTeamGoals < secondTeamGoals && userFirstGoal < userSecondGoal) ? 1 : 0;
-    }
-}
