8979740075ead3eaa3b6f936646cffd7f822aab0
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2019-04-15 17:26:53 +0300

    remove from hw09

--- src/com/ua/mate/hw07/di/Component.java
+++
@@ -1,8 +0,0 @@
-package com.ua.mate.hw07.di;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Component {
-}
--- src/com/ua/mate/hw09/Sendable.java
+++
@@ -1,8 +0,0 @@
-package com.ua.mate.hw09;
-
-public abstract class Sendable<T> {
-
-    public abstract String getTo();
-
-    public abstract T getContent();
-}
