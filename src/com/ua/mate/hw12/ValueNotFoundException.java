11040e6379a543559815177633faae080c48c5fe
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2019-04-20 20:11:37 +0300

    remove from hw12

--- src/com/ua/mate/hw12/ValueNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ua.mate.hw12;
-
-public class ValueNotFoundException extends Throwable {
-    public ValueNotFoundException(String value_not_found) {
-        System.out.println(value_not_found);
-    }
-}
