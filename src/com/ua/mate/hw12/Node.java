11f857dcb5a622749349a04c54009e22aaeb515d
Ruslan Malyavsky <ruslan-gorod@ukr.net>
2019-04-20 20:10:40 +0300

    hw12

---
+++ src/com/ua/mate/hw12/Node.java
@@ -0,0 +1,52 @@
+package com.ua.mate.hw12;
+
+import java.util.Objects;
+
+public class Node<K, V> {
+    final int hash;
+    final K key;
+    V value;
+    Node<K, V> next;
+
+    Node(int hash, K key, V value, Node<K, V> next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public final V setValue(V newValue) {
+        V oldValue = value;
+        value = newValue;
+        return oldValue;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?, ?> node = (Node<?, ?>) o;
+        return Objects.equals(key, node.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
+    }
+}
